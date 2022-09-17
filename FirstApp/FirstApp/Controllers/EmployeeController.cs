2c10846d65cb6d5dbbadd59f8169c3a59472653c
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-18 03:16:37 +0530

    Nitish Mehta added ASP.NET Core CRUD operations

---
+++ FirstApp/FirstApp/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // HTTP GET VERSION
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            Repository.Create(employee);
+            return View("Thanks", employee);
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace FirstApp.Models
+{
+    public static class Repository
+    {
+        private static List<Employee> allEmpoyees = new List<Employee>();
+        public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+        public static void Create(Employee employee)
+        {
+            allEmpoyees.Add(employee);
+        }
+    }
+}
