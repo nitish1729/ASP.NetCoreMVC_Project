7959086f5633b000da735bd5c75f202688b36a4e
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-18 02:23:19 +0530

    first application ASP.NET Core 6.0 MVC app

---
+++ FirstApp/FirstApp/Controllers/FirstController.cs
@@ -0,0 +1,26 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World Wellcome to ASP.Net Core MVC 6";
+        }
+        public IActionResult Hello()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.Name = "Nitish Mehta";
+            person.Age = 25;
+            person.Location = "Bangalore";
+            return View(person);
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FirstApp.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+        public string Location { get; set; } = String.Empty;
+        public void NaturalNumber()
+        {
+            Console.WriteLine("Enter the number");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
