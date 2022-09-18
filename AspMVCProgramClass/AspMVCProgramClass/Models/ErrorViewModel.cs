02f9bbc2702edb212c07e40c3dd8710ed7e99b71
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-19 02:28:22 +0530

    added new practice section

---
+++ AspMVCProgramClass/AspMVCProgramClass/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspMVCProgramClass.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
