4e51577e237cde60f2ce8dee052e6d657f7b8461
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-19 13:35:18 +0530

    created MVC folder and Files manually

---
+++ MyFirstWebAppEmptyfiles/MyFirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("Nitish Mehta");
+            return View();
+        }
+        public IActionResult navbar()
+        {
+            return View();
+        }
+        public IActionResult name()
+        {
+            Console.WriteLine("Nitish Mehta");
+            return View();
+           
+        }
+    }
+}
