2805fa2235f9aaa7122b6b795a0ce9c12ec70319
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-21 01:33:35 +0530

    sending data through Post Method

---
+++ MyFirstWebAppEmptyfiles/MyFirstWebApp/Controllers/PostMethodController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class PostMethodController : Controller
+    {
+        public IActionResult PostMethodData()
+        {
+            return View();
+        }
+    }
+}
