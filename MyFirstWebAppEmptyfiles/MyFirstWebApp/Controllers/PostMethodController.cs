fdd8e76e3417a82466baa8426e4c7564e58b93b4
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-21 02:24:46 +0530

    sending data through Post Method

--- MyFirstWebAppEmptyfiles/MyFirstWebApp/Controllers/PostMethodController.cs
+++ MyFirstWebAppEmptyfiles/MyFirstWebApp/Controllers/PostMethodController.cs
@@ -8,5 +8,35 @@ namespace MyFirstWebApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        
+        public IActionResult PostMethodData(string Email_Id, string Password,string Present_Address, string Residential_Address, string City, string State, int Zip)
+        {
+            string MyemailId = Email_Id;
+            string MyPassword = Password;
+            string PAddress = Present_Address;
+            string RAddress = Residential_Address;
+            string MyCity = City;
+            string MyState = State;
+            int ZipCode = Zip;
+
+            return View();
+        }
+
+        /*
+        public IActionResult Postmethod(string Email_Id, string Password, string Present_Address, string Residential_Address, string City, string State, int Zip)
+        {
+            string MyemailId = Email_Id;
+            string MyPassword = Password;
+            string PAddress = Present_Address;
+            string RAddress = Residential_Address;
+            string MyCity = City;
+            string MyState = State;
+            int ZipCode = Zip;
+
+            return View("PostMethodData");
+           
+        }
+        */
     }
 }
