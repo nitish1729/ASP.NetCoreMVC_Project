866e01cdbb8119ee9679a2868eef422ac6b19bb8
Nitish Mehta <nkrmehta09@gmail.com>
2022-09-25 19:49:15 +0530

    Private Access modifer

---
+++ C_Csharp/ClassLibrary1AssemblyOne/AssemblyOneClass1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    public class AssemblyOneClass1
+    {
+        private int Id;
+        public void Display1()
+        {
+            //Private Member Accessible with the Containing Type only
+            //Where they are created, they are available only within that type 
+            Console.WriteLine(Id);
+        }
+    }
+    public class AssemblyOneClass2 : AssemblyOneClass1
+    {
+        public void Display2()
+        {
+            //You cannot access the Private Member from the Derived Class
+            //Within the Same Assembly
+           Console.WriteLine(Id); //Compile Time Error
+        }
+    }
+    public class AssemblyOneClass3
+    {
+        public void Dispplay3()
+        {
+            //You cannot access the Private Member from the Non-Derived Classes
+            //Within the Same Assembly
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+           Console.WriteLine(obj.Id); //Compile Time Error
+        }
+    }
+}
