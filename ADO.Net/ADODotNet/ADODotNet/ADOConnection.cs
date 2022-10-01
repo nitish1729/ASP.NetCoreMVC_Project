9f0d4a43fa76cf1140d8d4425bd3736c756f71b4
Nitish Mehta <nkrmehta09@gmail.com>
2022-10-02 02:17:14 +0530

    RENAME A TABLE USING C# ADO.NET

--- ADO.Net/ADODotNet/ADODotNet/ADOConnection.cs
+++ ADO.Net/ADODotNet/ADODotNet/ADOConnection.cs
@@ -15,22 +15,16 @@ namespace ADODotNet
             string cs = @"Data Source=DESKTOP-C3BQOGP\SQLEXPRESS;Initial Catalog=ComputerShop;Integrated Security = true;";
             SqlConnection conn = null;
             conn = new SqlConnection(cs);
-            //CREATE A TABLE USING C# ADO.NET
-            string query = @"CREATE TABLE dbo.Products
-                (
-                    ID int IDENTITY(1,1) NOT NULL,
-                    Name nvarchar(50) NULL,
-                    Price nvarchar(50) NULL,
-                    Date datetime NULL,
-                    CONSTRAINT pk_id PRIMARY KEY (ID)
-                );";
+            //RENAME A TABLE USING C# ADO.NET
+            string query = @"EXEC sp_rename 'Products', 'Accessories'";
+
             SqlCommand cmd = new SqlCommand(query, conn);
 
             try
             {
                 conn.Open();
                 cmd.ExecuteNonQuery();
-                Console.WriteLine("Table Created Successfully");
+                Console.WriteLine("Table Renamed Successfully");
             }
             catch(SqlException e)
             {
