49cbd710f32db20a7ea9c03b4e4307432559be6a
Nitish Mehta <nkrmehta09@gmail.com>
2022-10-02 03:17:46 +0530

    Coping the records of Products into Items table

--- ADO.Net/ADODotNet/ADODotNet/ADOConnection.cs
+++ ADO.Net/ADODotNet/ADODotNet/ADOConnection.cs
@@ -15,18 +15,30 @@ namespace ADODotNet
             string cs = @"Data Source=DESKTOP-C3BQOGP\SQLEXPRESS;Initial Catalog=ComputerShop;Integrated Security = true;";
             SqlConnection conn = null;
             conn = new SqlConnection(cs);
-            //Replaced Parameters with Value
-            string query = "INSERT INTO Products (Name, Price, Date) VALUES(@Name, @Price, @Date)";
+            //Created a more table
+            //string query = "INSERT INTO Products (Name, Price, Date) VALUES(@Name, @Price, @Date)";
+            /*
+            string query = @"create table dbo.Items
+              (
+               Name varchar(50) NULL,
+               Price varchar(50) NULL,
+               Date datetime NULL,
+               );";
+            */
+            // Coping the records of Products into Items table
+            string query = "INSERT INTO Items(Name,Price,Date) SELECT Name,Price,Date FROM Products";
             SqlCommand cmd = new SqlCommand(query, conn);
+            /*
             //Pass values to Parameters
             cmd.Parameters.AddWithValue("@Name", "USB Keyboard");
             cmd.Parameters.AddWithValue("@Price", "$20");
             cmd.Parameters.AddWithValue("@Date", "25 May 2017");
+            */
             try
             {
                 conn.Open();
                 cmd.ExecuteNonQuery();
-                Console.WriteLine("Records Inserted Successfully");
+                Console.WriteLine("Records copied into Items table Successfully");
             }
             catch(SqlException e)
             {
