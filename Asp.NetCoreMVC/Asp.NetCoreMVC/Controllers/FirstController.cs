20bf8536400212ac2a522aae8c428ee371799603
Nitish Mehta <nkrmehta09@gmail.com>
2022-10-05 11:59:17 +0530

    Adding Model and transferring it’s data to the View

--- Asp.NetCoreMVC/Asp.NetCoreMVC/Controllers/FirstController.cs
+++ Asp.NetCoreMVC/Asp.NetCoreMVC/Controllers/FirstController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCoreMVC.Controllers
 {
@@ -11,7 +12,18 @@ namespace Asp.NetCoreMVC.Controllers
 
         public IActionResult Hello()
         {
+            ViewBag.Message = "Hello World Wellcome to the ASP.Net Core MVC ";
             return View();
+        }
+        public IActionResult info()
+        {
+            Person person = new Person();
+            person.FirstName = "Nitish";
+            person.LastName = " Mehta";
+            person.FullName = person.FirstName + " " + person.LastName;
+            person.age = 25;
+            person.city = "Bangalore";
+            return View(person);
         }
     }
 }
---
+++ Asp.NetCoreMVC/Asp.NetCoreMVC/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Asp.NetCoreMVC.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string FullName { get; set; } = String.Empty;
+        public int age { get; set; }
+        public string city { get; set; } = String.Empty;
+    }
+}
