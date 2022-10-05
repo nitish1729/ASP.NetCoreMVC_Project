20158ed0b93c3aca871f7560a79d0061b40ee164
Nitish Mehta <nkrmehta09@gmail.com>
2022-10-05 11:02:12 +0530

    Action method returning View

---
+++ Asp.NetCoreMVC/Asp.NetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Action method returning View";
+        }
+        public string Index1()
+        {
+            return "Hello World";
+        }
+        public string show()
+        {
+            return "Nitish Mehta";
+        }
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+    }
+}
